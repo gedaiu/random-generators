6c147d431965e1b90559094ecfe71cb1e78bf665
Alexandra <alexandra.casapu@softhouse.se>
2015-09-12 14:24:35 +0300

    added newly created file

---
+++ source/generators/ascending.d
@@ -0,0 +1,24 @@
+module generators.ascending;
+
+public import std.range;
+
+import tested;
+import israndomgenerator;
+import std.random;
+import std.algorithm.comparison;
+
+double previous = 0;
+
+auto ascending() {
+  auto random = uniform(previous, min(previous+0.001,1));
+  previous = random;
+  return random;
+}
+
+@name("it should return false for generator which always returns a value equal to or larger than the previous")
+unittest {
+  auto generator = generate!ascending();
+
+  auto result = isRandomGenerator(generator);
+  assert(result == false);
+}
