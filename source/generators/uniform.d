0f9f328e2c5671c63c1a164103bcc2eeaed082c1
Szabo Bogdan <szabobogdan@yahoo.com>
2015-09-12 17:07:00 +0300

    added uniform generator

---
+++ source/generators/uniform.d
@@ -0,0 +1,25 @@
+module generators.uniform;
+
+public import std.range;
+
+import tested;
+import israndomgenerator;
+import std.algorithm.comparison;
+
+ulong old = ulong.max / 4;
+
+auto uniform(ulong a, ulong b)() {
+
+  old = a * old + b;
+
+  return (cast(double) old / cast(double) ulong.max);
+}
+
+@name("it should return true for generator which returns uniform distribution")
+unittest {
+  alias customUniform = uniform!(36969, 18000);
+  auto generator = generate!customUniform();
+
+  auto result = isRandomGenerator(generator);
+  assert(result == true);
+}
