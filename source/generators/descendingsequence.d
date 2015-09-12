f611a8ee730a4d864ef308fa11bf665132a39bfe
Szabo Bogdan <szabobogdan@yahoo.com>
2015-09-12 14:39:12 +0300

    sequence of descending numbers

---
+++ source/generators/descendingsequence.d
@@ -0,0 +1,23 @@
+module generators.descendingsequence;
+
+public import std.range;
+
+import tested;
+import israndomgenerator;
+import std.algorithm.comparison;
+
+double previous = 10;
+
+auto descendingSequence() {
+  auto number = previous / 10;
+  previous = previous - 1 == -1 ? 10 : previous - 1;
+  return number;
+}
+
+@name("it should return false for generator which returns the same sequence of descending numbers")
+unittest {
+  auto generator = generate!descendingSequence();
+
+  auto result = isRandomGenerator(generator);
+  assert(result == false);
+}
